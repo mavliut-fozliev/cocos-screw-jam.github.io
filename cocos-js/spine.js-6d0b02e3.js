1b14b1db17f513f776110f1ab9a5685021f334e7
JamesBerlioz <mavlut18@gmail.com>
2024-08-26 00:01:33 +0300

    feat: update imports

--- cocos-js/spine.js-6d0b02e3.js
+++ cocos-js/spine.js-6d0b02e3.js
@@ -1,1 +1,8 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-90a4faeb.bin")}}}));
+System.register([], function (e) {
+  "use strict";
+  return {
+    execute: function () {
+      e("default", "./assets/spine.js.mem-90a4faeb.bin");
+    },
+  };
+});
